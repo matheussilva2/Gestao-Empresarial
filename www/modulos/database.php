2106c547ae42f3706f5d2615a9b6df60caef7527
Matheus Silva <matheusnascimentoeoq@gmail.com>
2019-05-08 21:27:02 -0300

    Separacao de config do banco de dados

--- www/modulos/database.php
+++ www/modulos/database.php
@@ -1,7 +1,9 @@
 <?php
+	require_once("config.php");
+	
 	function conectar(){
 		try {
-			$conexao = new PDO('mysql:host=localhost;dbname=argila_mais','admin', '@rg1l@m@1s1346',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+			$conexao = new PDO('mysql:host='.HOST.';dbname='.DATABASE,USER, PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 			return $conexao;
 		} catch (PDOException $e) {
 			die($e);
